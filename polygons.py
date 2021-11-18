8b8e7e02b78b257721fbb79554e7ca5be89300e8
Abhishek Dharmadhikari <abhishekdharmadhikari25@gmail.com>
2021-11-18 11:25:41 +0530

    All test cases handled + Code-formaating Done

--- polygons.py
+++ polygons.py
@@ -39,11 +39,11 @@ class polygon:
         if(not type(length_of_side) is float and not type(length_of_side) is int):
             raise Exception('Invalid data type for length_of_each_side:{}'.format(type(length_of_side)))
         elif(no_of_sides in [0,1,2,360]):
-            raise Exception ("Polygon must have number of sides in [3,360).You entered',no_of_sides")
+            raise Exception('Polygon must have number of sides in [3,360)!You entered {}'.format(no_of_sides))
         elif(no_of_sides < 0 or length_of_side<0):
-            raise Exception("Polygon cannot have negative number of sides.")
+            raise Exception('No of sides and Length of side should be Positive!')
         elif(length_of_side==0):
-            raise Exception('Polygon cannot have side with length 0!')
+            raise Exception('l(side) should be non zero positive.')
         else:
             n = no_of_sides
             self.predict_shape(n)
@@ -58,30 +58,27 @@ class polygon:
 
     def use_interior_angle(self,interior_angle,length_of_side):
         if(not type(interior_angle) is int and not type(interior_angle) is float):
-            raise Exception('Interior angle should be integer or float')
+            raise Exception('Interior angle should be integer or float.')
         if(not type(length_of_side) is float and not type(length_of_side) is int):
             raise Exception('Length of side should integer or float and type you given is :',type(length_of_side))
         if(interior_angle >=180 or interior_angle<=0):
             raise Exception("Interior angle should in range (0,180)!")
         elif(length_of_side<=0):
-            raise Exception('Length of side <= 0 which is invalid!')
+            raise Exception('l(side) should be non zero positive.')
         #interior_angle = 180 * (n-2)/n
         #interior_angle / 180 = 1-2/n i.e
         # 1- (interior_angle/180) = 2/n 
         #i.e n = 2 / (1-interior_angle/180) then if n is integer then it is valid
         
         temp = interior_angle / 180
-        print(temp)
         temp = 1 - temp
-        print(temp)
-        print(interior_angle,temp)
         n = 2/temp
         n = round(n,3)
         if( n - int(n) == 0):
             self.predict_shape(n)
             self.polygon_features(n,length_of_side)
         else:
-            raise Exception('From calculation, We got as {} number of sides'.format(n))
+            raise Exception('No of sides: {} which is float(from interior angle calculations!)'.format(n))
         features=["","","","",""]
         features[0]=self.shape_predict
         features[1]=self.area
@@ -92,19 +89,19 @@ class polygon:
 
     def use_exterior_angle(self,exterior_angle,length_of_side):
         if(not type(exterior_angle) is int and not type(exterior_angle) is float):
-            raise Exception('Wrong input type is provided!')
+            raise Exception('Exterior angle should be integer or float.')
         if(not type(length_of_side) is float and not type(length_of_side) is int):
-            raise Exception('Length of side should integer or float')
+            raise Exception('Length of side should integer or float.')
         if(exterior_angle >=180 or exterior_angle<=0):
-            raise Exception("You have entered invalid exterior angle(not in range(0,180))")
+            raise Exception("You have entered invalid exterior angle(not in range(0,180)).")
         elif(length_of_side<=0):
-            raise Exception('Polygon cannot have side with length <=0!Input valid length')
+            raise Exception('l(side) should be non zero positive.')
         n = float(360/exterior_angle)
         if( n - int(n) == 0):
             self.predict_shape(n)
             self.polygon_features(n,length_of_side)
         else:
-            raise Exception('We are getting number of sides as float which is impossible!You entered Invalid  interior angle')
+            raise Exception('No of sides: {} which is float(from exterior angle calculations)'.format(n))
         features=["","","","",""]
         features[0]=self.shape_predict
         features[1]=self.area
@@ -117,15 +114,15 @@ class polygon:
     def irregular_polygon(self,*args):
         n = len(args)
         if(n in [0,1,2]):
-            raise Exception('Polygon must have number of sides>=3')
+            raise Exception('You provided {} sides.Polygon must have number of sides>=3.'.format(n))
         elif(n >=5):
-            raise Exception('Currently not available')
+            raise Exception('You entered {} arguments.Difficult to predict features'.format(n))
         if n==3:
             for x in args:  
                 if(not type(x) is int and not type(x) is float):
-                    raise Exception("Unsupported Data type")   
+                    raise Exception("Unsupported Data type!")   
                 if(x<=0):
-                    raise Exception("Polygon with 3 sides not possible(length<=0)!")
+                    raise Exception("Triangle cannot have l(side) <=0!")
             a = args[0]
             b = args[1]
             c = args[2]
@@ -134,9 +131,9 @@ class polygon:
             dict[b]+=1
             dict[c]+=1
             if(a<=0 or b<=0 or c<=0):
-                raise Exception("Triangle with <=0 sides found") 
+                raise Exception("Triangle cannot have l(side)<=0 found") 
             elif(a+b<=c or a+c<=b or b+c<=a):
-                raise Exception("Basic Triangle property lost")
+                raise Exception("Basic Triangle property lost!")
             else:
                 if(len(dict)==1):
                     self.shape_predict = 'Equilateral Triangle'
@@ -157,7 +154,7 @@ class polygon:
                 if(not type(x) is int and not type(x) is float):
                     raise Exception("Unsupported Data type")   
                 if(x<=0):
-                    raise Exception("Polygon with 4 sides (length<=0)!")
+                    raise Exception("l(side) must be Non zero positive!")
                 dict[x]+=1
             if(len(dict)==1):
                 self.shape_predict = 'Square'
@@ -171,23 +168,11 @@ class polygon:
                     self.perimeter +=2*side
                     self.area *= side
             else:
-                self.shape_predict = 'Polygon with 4 sides!(neither rectangle nor square)'
+                self.shape_predict = 'It is quadrilateral!(neither rectangle nor square)'
                 self.perimeter = args[0] + args[1] + args[2] + args[3]
                 self.area = "difficult to find!"
             features=[]
             features.append(self.shape_predict)
             features.append(self.area)
             features.append(self.perimeter)
-            
         return features
-'''
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKCYAN = '\033[96m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
-'''
--- testing.py
+++ testing.py
@@ -9,23 +9,24 @@ invalid = '\033[91m' # Red
 endc = '\033[0m'
 yellow = '\033[93m'
 blue = '\033[94m'
+header = '\033[95m'
 
 # TABLE : TEST_CASE:  | Arguments |  Output  |
 class testing_polygon():
     def print_features(self,features):
-        f=""
-        f+='Shape: '+features[0]+'\n'
-        f+='Area: '+str(features[1])+'\n'
-        f+='Perimeter: '+str(features[2])+'\n'
-        f+='Interior_angle: '+str(features[3])+'\n'
-        f+='Exterior_angle: '+str(features[4])+'\n'
+        f=valid
+        f+='Shape: '+features[0]+endc+'\n'
+        f+=valid+'Area: '+str(features[1])+endc+'\n'
+        f+=valid+'Perimeter: '+str(features[2])+endc+'\n'
+        f+=valid+'Interior_angle: '+str(features[3])+endc+'\n'
+        f+=valid+'Exterior_angle: '+str(features[4])+endc+'\n'
         return f
 
     def print_features_1(self,features):
-        f=""
-        f+='Shape: '+features[0]+'\n'
-        f+='Area: '+str(features[1])+'\n'
-        f+='Perimeter: '+str(features[2])+'\n'
+        f=valid
+        f+=valid+'Shape: '+features[0]+endc+'\n'
+        f+=valid+'Area: '+str(features[1])+endc+'\n'
+        f+=valid+'Perimeter: '+str(features[2])+endc+'\n'
         return f
 
     def testing_using_sides(self):
@@ -33,6 +34,19 @@ class testing_polygon():
         test_table = PrettyTable([blue + "TEST_CASE"+ endc, blue + "arguments" + endc,blue + "Output" + endc])
         test_table.align = 'l'
         p1 = polygon()
+        try:
+            features = p1.sides_length(5,8,10)
+        except Exception as e:
+            test_table.add_row(["extra arguments provided:", "N=5, L=8 , arg=10",invalid + str(e) + endc])
+        test_table.add_row(['','',''])
+
+        try:
+            features = p1.sides_length()
+        except Exception as e:
+            test_table.add_row(["No arguments provided:", "empty",invalid + str(e) + endc])
+        test_table.add_row(['','',''])
+
+
         try:
             features = p1.sides_length(6.2,8)
         except Exception as e:
@@ -64,9 +78,10 @@ class testing_polygon():
             test_table.add_row(["Length of side:0", "N=6,L=0",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
-            features=p1.sides_length(5,'a')
+            features=p1.sides_length(4,8)
+            test_table.add_row([yellow+"Valid input:"+endc, "N=4,L=8",self.print_features(features)])
         except Exception as e:
-            test_table.add_row(["Character input:", "N=5,L='a'",invalid + str(e) + endc])
+            test_table.add_row(["Invalid input:", "N=4,L=8",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             features=p1.sides_length(5,-44)
@@ -75,7 +90,7 @@ class testing_polygon():
         test_table.add_row(['','',''])
         try:
             features=p1.sides_length(6,10)
-            test_table.add_row(["Valid input", "N=6,L=10",valid + self.print_features(features) + endc])
+            test_table.add_row([yellow+"Valid input"+endc, "N=6,L=10",self.print_features(features)])
         except Exception as e:
              test_table.add_row(["Failed!", "N=6,L=10", invalid + str(e) + endc])
         print(test_table)
@@ -87,6 +102,18 @@ class testing_polygon():
 
         p1 = polygon()
         try:
+            features = p1.use_interior_angle(125,8,10)
+        except Exception as e:
+            test_table.add_row(["extra arguments provided:", "measure=125\u00b0, L=8 , arg=10",invalid + str(e) + endc])
+        test_table.add_row(['','',''])
+
+        try:
+            features = p1.use_interior_angle()
+        except Exception as e:
+            test_table.add_row(["No arguments provided:", "empty",invalid + str(e) + endc])
+        test_table.add_row(['','',''])
+
+        try:
             p1.use_interior_angle(190,3)
         except Exception as e:
             test_table.add_row(["Interior angle beyond 180\u00b0:", "measure=190\u00b0,side=3",invalid + str(e) + endc])
@@ -94,12 +121,12 @@ class testing_polygon():
         try:
             p1.use_interior_angle(0,4)
         except Exception as e:
-            test_table.add_row(["Zero input:", "measure=0\u00b0,side=4",invalid + str(e) + endc])
+            test_table.add_row(["Zero input(1):", "measure=0\u00b0,side=4",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             p1.use_interior_angle(-12,8)
         except Exception as e:
-            test_table.add_row(["Negative input:", "measure=-12\u00b0,side=3",invalid + str(e) + endc])
+            test_table.add_row(["Negative input:(1)", "measure=-12\u00b0,side=3",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             p1.use_interior_angle('string',6)
@@ -109,12 +136,12 @@ class testing_polygon():
         try:
             p1.use_interior_angle(120,0)
         except Exception as e:
-            test_table.add_row(["length of side(zero):", "measure=120\u00b0,side=0",invalid + str(e) + endc])
+            test_table.add_row(["Zero input(2):", "measure=120\u00b0,side=0",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             p1.use_interior_angle(90,-2)
         except Exception as e:
-           test_table.add_row(["Negative length of side:", "measure=90\u00b0,side=-2",invalid + str(e) + endc])
+            test_table.add_row(["Negative Input:(2)", "measure=90\u00b0,side=-2",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             p1.use_interior_angle(72,5)
@@ -123,12 +150,12 @@ class testing_polygon():
         test_table.add_row(['','',''])  
         try:
             features=p1.use_interior_angle(108,6)
-            test_table.add_row(["Valid input:", "measure=108\u00b0,side=6",valid + self.print_features(features) + endc])
+            test_table.add_row([yellow+"Valid input:"+endc, "measure=108\u00b0,side=6",valid + self.print_features(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "measure=108,side=6",invalid + str(e) + endc])
         try:
             features=p1.use_interior_angle(144,12)
-            test_table.add_row(["Valid input:", "measure=108\u00b0,side=6", valid + self.print_features(features) + endc])
+            test_table.add_row([yellow+"Valid input:"+endc, "measure=108\u00b0,side=6", valid + self.print_features(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "measure=108,side=6",invalid + str(e) + endc])
         
@@ -142,6 +169,18 @@ class testing_polygon():
 
         p1 = polygon()
         try:
+            features = p1.use_exterior_angle(65,5,4)
+        except Exception as e:
+            test_table.add_row(["extra arguments provided:", "measure=65\u00b0, L=5 , arg=4",invalid + str(e) + endc])
+        test_table.add_row(['','',''])
+
+        try:
+            features = p1.use_exterior_angle()
+        except Exception as e:
+            test_table.add_row(["No arguments provided:", "empty",invalid + str(e) + endc])
+        test_table.add_row(['','',''])
+
+        try:
             p1.use_exterior_angle(210,3)
         except Exception as e:
             test_table.add_row(["Exterior angle out of range:", "measure=210\u00b0,side=3",invalid + str(e) + endc])
@@ -154,7 +193,7 @@ class testing_polygon():
         try:
             p1.use_exterior_angle(-35,8)
         except Exception as e:
-            test_table.add_row(["Negative input:", "measure=-35\u00b0,side=8",invalid + str(e) + endc])
+            test_table.add_row(["Negative input:(1)", "measure=-35\u00b0,side=8",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             p1.use_exterior_angle('string11',6)
@@ -169,17 +208,17 @@ class testing_polygon():
         try:
             p1.use_exterior_angle(60,-3)
         except Exception as e:
-            test_table.add_row(["Negative input:", "measure=60\u00b0,side=-3",invalid + str(e) + endc])
+            test_table.add_row(["Negative input:(2)", "measure=60\u00b0,side=-3",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             features = p1.use_exterior_angle(90,5)
-            test_table.add_row(["Valid input:", "measure=90\u00b0,side=5", valid + self.print_features(features) + endc])
+            test_table.add_row([yellow+"Valid input:"+endc, "measure=90\u00b0,side=5", valid + self.print_features(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "measure=90\u00b0,side=5",invalid + str(e) + endc])
         test_table.add_row(['','',''])    
         try:
             features=p1.use_exterior_angle(72,8)
-            test_table.add_row(["Valid input:", "measure=72\u00b0,side=8",valid + self.print_features(features) + endc])
+            test_table.add_row([yellow+"Valid input:"+endc, "measure=72\u00b0,side=8",valid + self.print_features(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "measure=72,side=8",invalid + str(e) + endc])
         print(test_table)
@@ -189,6 +228,18 @@ class testing_polygon():
         test_table = PrettyTable([blue + "TEST_CASE"+ endc, blue + "arguments" + endc,blue + "Output" + endc])
         test_table.align = 'l'
         p1=polygon()
+        try:
+            features = p1.irregular_polygon(10,10,20,12,12)
+        except Exception as e:
+            test_table.add_row(["extra arguments provided:", "sides are : 10,10,20,12,12",invalid + str(e) + endc])
+        test_table.add_row(['','',''])
+
+        try:
+            features = p1.irregular_polygon()
+        except Exception as e:
+            test_table.add_row(["No arguments provided:", "empty",invalid + str(e) + endc])
+        test_table.add_row(['','',''])
+
         try:
             features=p1.irregular_polygon(0,2,1)
         except Exception as e:
@@ -211,31 +262,31 @@ class testing_polygon():
         test_table.add_row(['','',''])
         try:
             features=p1.irregular_polygon(3,3,3)
-            test_table.add_row(["Valid Input:", "N=3,3,3", valid + self.print_features_1(features) + endc])
+            test_table.add_row([yellow+"Valid Input:"+endc, "sides are:3,3,3", valid + self.print_features_1(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "No=3,3,3",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             features=p1.irregular_polygon(3,2,4)
-            test_table.add_row(["Valid input:", "N=3,2,4", valid + self.print_features_1(features) + endc])
+            test_table.add_row([yellow+"Valid input:"+endc, "sides are: 3,2,4", valid + self.print_features_1(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "sides are : 3,2,4",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             features=p1.irregular_polygon(3,3,4)
-            test_table.add_row(["Valid input:", "N=3,3,4", valid + self.print_features_1(features) + endc])
+            test_table.add_row([yellow+"Valid input:"+endc, "sides are :3,3,4", valid + self.print_features_1(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "sides are : 3,3,4",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             features=p1.irregular_polygon(4,3,4,3)
-            test_table.add_row(["Valid input:", "N=4,3,4,3", valid + self.print_features_1(features) + endc])
+            test_table.add_row([yellow+"Valid input:"+endc, "sides are:4,3,4,3", valid + self.print_features_1(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "sides are :4,3,4,3",invalid + str(e) + endc])
         test_table.add_row(['','',''])
         try:
             features=p1.irregular_polygon(4,4,4,4)
-            test_table.add_row(["Valid input:", "N=4,4,4,4", valid + self.print_features_1(features) + endc])
+            test_table.add_row([yellow+"Valid input:"+endc, "sides are :4,4,4,4", valid + self.print_features_1(features) + endc])
         except Exception as e:
             test_table.add_row(["Invalid input:", "sides are :4,4,4,4",invalid + str(e) + endc])
         test_table.add_row(['','',''])
@@ -248,7 +299,32 @@ class testing_polygon():
 
 
 c = testing_polygon()
-c.testing_using_sides()
-c.testing_using_interior_angle()
-c.testing_using_exterior_angle()
-c.testing_using_irregular_polygon()
+def display_menu():
+    menu_bar = header
+    menu_bar+="1.Test function where no of sides and length of side is given\n"
+    menu_bar+="2.Test function where interior angle and length of side is given.\n"
+    menu_bar+="3.Test function where exterior angle and length of side is given.\n"
+    menu_bar+="4.Testing function for irregular polygon:(for triangles and quadrilaterals)\n"
+    menu_bar+=endc
+    print(menu_bar)
+    return
+
+choice = 'y'
+while (choice =='y' or choice =='yes'):
+    display_menu()
+    opt = int(input("Select option_no from menu:"))
+    if(opt==1):
+        print(yellow+"Testing for sides and length function:"+endc)
+        c.testing_using_sides()
+    elif(opt==2):
+        print(yellow+"Testing using Interior angle:"+endc)
+        c.testing_using_interior_angle()
+    elif(opt==3):
+        print(yellow+"Testing using Exterior angle:"+endc)
+        c.testing_using_exterior_angle()
+    elif(opt==4):
+        print(yellow+"Testing for any type of triangle and polygon with 4 sides:"+endc)
+        c.testing_using_irregular_polygon()
+    else:
+        print(invalid + 'Invalid choice'+endc)
+    choice = input("You want to proceed or not?:")
